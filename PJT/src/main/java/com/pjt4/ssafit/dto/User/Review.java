28fe5b822311f1ebfb191787fc764745d77c7ca3
gcy9293 <gcy9293@naver.com>
2025-09-26 17:17:03 +0900

    update Review 기능

--- PJT/src/main/java/com/pjt4/ssafit/dto/User/Review.java
+++ PJT/src/main/java/com/pjt4/ssafit/dto/User/Review.java
@@ -1,5 +1,90 @@
 package com.pjt4.ssafit.dto.User;
 
-public class Review {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Review implements Serializable{
+	private static int no=1;
+	private int id;
+	private String title;
+	private String writer;
+	private String content;
+	private String regDate;
+	private int viewCnt;
+	
+	
+	public Review() {
+	}
+	
+	public Review(String title, String writer, String content) {
+		this.id = no++;
+		this.title = title;
+		this.writer = writer;
+		this.content = content;
+		this.viewCnt = 0;
+		this.regDate = new Date().toString();
+	}
+	
+	public static int getNo() {
+		return no;
+	}
+
+	public static void setNo(int no) {
+		Review.no = no;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getViewCnt() {
+		return viewCnt;
+	}
+
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", viewCnt=" + viewCnt + "]";
+	}
 
 }
--- PJT/src/main/java/com/pjt4/ssafit/service/ReviewService.java
+++ PJT/src/main/java/com/pjt4/ssafit/service/ReviewService.java
@@ -1,5 +1,23 @@
 package com.pjt4.ssafit.service;
 
+import java.util.List;
+
+import com.pjt4.ssafit.dto.User.Review;
+
 public interface ReviewService {
+	// 전체 게시글 조회
+	public abstract List<Review> getList();
+
+	// 게시글 조회
+	public abstract Review getReview(int id);
+
+	// 게시글 작성
+	public abstract void writeReview(Review review);
+
+	// 게시글 수정
+	public abstract void modifyReview(Review review);
+
+	// 게시글 삭제
+	public abstract void removeReview(int id);
 
 }
